4c76abb6e1df87a68d5fcd0630cbc9103c4916e3
shlok1999 <09shlok1999ae1@gmail.com>
2022-02-15 21:43:35 +0530

    added background color

---
+++ app/src/main/java/com/example/relatewithlove/IntroductoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.relatewithlove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroductoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introductory)
+    }
+}
