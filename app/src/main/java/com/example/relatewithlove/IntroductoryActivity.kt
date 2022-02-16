f42c20d7aaab33b541d721b6fc79e612f7125246
shlok1999 <09shlok1999ae1@gmail.com>
2022-02-16 14:53:48 +0530

    splash screen added

--- app/src/main/java/com/example/relatewithlove/IntroductoryActivity.kt
+++ app/src/main/java/com/example/relatewithlove/IntroductoryActivity.kt
@@ -1,11 +1,21 @@
 package com.example.relatewithlove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.os.postDelayed
 
 class IntroductoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introductory)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 300)
     }
 }
